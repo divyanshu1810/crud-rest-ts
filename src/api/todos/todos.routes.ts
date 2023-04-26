92a4b62d758c8878c7a8e52c26b47cfc646501c1
Divyanshu Yadav <dd8353@srmist.edu.in>
2023-04-26 16:38:17 +0530

    :art: Code Structured

---
+++ src/api/todos/todos.routes.ts
@@ -0,0 +1,13 @@
+import { Router, Response, Request } from 'express';
+import Todo from './todos.model';
+
+const router = Router();
+
+router.get('/', (req:Request, res:Response<Todo[]>) => {
+  res.json([{
+    content: 'Hello World',
+    done: false,
+  }]);
+});
+
+export default router;
