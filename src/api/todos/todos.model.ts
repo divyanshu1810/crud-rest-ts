6dd8f7f62b706c1f65db02fd6c56eec2a70547b6
Divyanshu Yadav <dd8353@srmist.edu.in>
2023-04-27 00:44:45 +0530

    :card_file_box: Database Added

--- src/api/todos/todos.model.ts
+++ src/api/todos/todos.model.ts
@@ -1,9 +1,12 @@
+import { WithId } from 'mongodb';
 import * as z from 'zod';
+import { db } from '../../db';
+
 const Todo  = z.object({
   content: z.string().min(1),
   done: z.boolean(),
 });
 
-type Todo = z.infer<typeof Todo>;
-
-export default Todo;
+export type Todo = z.infer<typeof Todo>;
+export type TodoWithId = WithId<Todo>;
+export const Todos = db.collection<Todo>('todos');
