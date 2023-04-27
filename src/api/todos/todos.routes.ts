c04a354fa24e24641fa1cf8f290a45baf7ba1fc0
Divyanshu Yadav <dd8353@srmist.edu.in>
2023-04-27 23:17:00 +0530

    :sparkles: Read Todo Added

--- src/api/todos/todos.routes.ts
+++ src/api/todos/todos.routes.ts
@@ -1,12 +1,7 @@
-import { Router, Response, Request } from 'express';
-import { Todos, TodoWithId  } from './todos.model';
-
+import { Router } from 'express';
+import * as TodoHandlers from './todos.handlers';
 const router = Router();
 
-router.get('/', async (req:Request, res:Response<TodoWithId[]>) => {
-  const result = await Todos.find();
-  const todos = await result.toArray();
-  res.json(todos);
-});
+router.get('/', TodoHandlers.findAll);
 
 export default router;
